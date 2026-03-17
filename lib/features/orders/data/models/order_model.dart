678aa910c7d2a58469e7eae276b64ecfb5cec955
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2026-03-17 07:16:56 -0700

    Create order model

---
+++ lib/features/orders/data/models/order_model.dart
@@ -0,0 +1,50 @@
+import 'order_product_model.dart';
+import 'shipping_address_model.dart';
+
+class OrderModel {
+  final double totalPrice;
+  final String uId;
+  final ShippingAddressModel shippingAddress;
+  final List<OrderProductModel> orderProducts;
+  final String paymentMethod;
+
+  OrderModel({
+    required this.totalPrice,
+    required this.uId,
+    required this.shippingAddress,
+    required this.orderProducts,
+    required this.paymentMethod,
+  });
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'totalPrice': totalPrice,
+      'uId': uId,
+      'status': 'pending',
+      'date': DateTime.now().toString(),
+      'shippingAddress': shippingAddress.toJson(),
+      'orderProducts': orderProducts.map((x) => x.toJson()).toList(),
+      'paymentMethod': paymentMethod,
+    };
+  }
+
+  factory OrderModel.fromJson(Map<String, dynamic> json) => OrderModel(
+    totalPrice: json['totalPrice'].toDouble(),
+    uId: json['uId'],
+    shippingAddress: ShippingAddressModel.fromJson(json['shippingAddress']),
+    orderProducts: List<OrderProductModel>.from(
+      json['orderProducts'].map((x) => OrderProductModel.fromJson(x)),
+    ),
+    paymentMethod: json['paymentMethod'],
+  );
+}
+
+
+/// total price
+/// uId
+/// shipping adress
+/// {}
+/// 
+/// list<orders>
+/// 
+/// payment method
