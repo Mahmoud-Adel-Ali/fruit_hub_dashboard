83657b3a9c76ea8a6721c0a203e5f5944a158d59
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2026-03-21 11:22:11 -0700

    232. Manage streams.

--- lib/features/orders/presentation/manager/fetch_orders_cubit/fetch_orders_cubit.dart
+++ lib/features/orders/presentation/manager/fetch_orders_cubit/fetch_orders_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -9,15 +11,29 @@ part 'fetch_orders_state.dart';
 class FetchOrdersCubit extends Cubit<FetchOrdersState> {
   FetchOrdersCubit({required this.repo}) : super(FetchOrdersInitial());
   final OrdersRepo repo;
+  StreamSubscription? _subscription;
 
-  void fetchOrders() async {
+  void fetchOrders() {
     emit(FetchOrdersLoading());
 
-    await for (var result in repo.fetchOrders()) {
+    _subscription = repo.fetchOrders().listen((result) {
       result.fold(
         (failure) => emit(FetchOrdersFailure(message: failure.message)),
         (orders) => emit(FetchOrdersSuccess(orders: orders)),
       );
-    }
+    });
+
+    // await for (var result in repo.fetchOrders()) {
+    //   result.fold(
+    //     (failure) => emit(FetchOrdersFailure(message: failure.message)),
+    //     (orders) => emit(FetchOrdersSuccess(orders: orders)),
+    //   );
+    // }
+  }
+
+  @override
+  Future<void> close() {
+    _subscription?.cancel();
+    return super.close();
   }
 }
