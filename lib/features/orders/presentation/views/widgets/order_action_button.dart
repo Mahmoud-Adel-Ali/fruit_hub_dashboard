96100a6213d5bdd34315b4865bf5301979db066b
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2026-03-25 08:32:58 -0700

    238. Update order status done.

--- lib/features/orders/presentation/views/widgets/order_action_button.dart
+++ lib/features/orders/presentation/views/widgets/order_action_button.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../../core/enums/order_enum.dart';
 import '../../../domain/entities/order_entity.dart';
+import '../../manager/update_order_cubit/update_order_cubit.dart';
 
 class OrderActionButtons extends StatelessWidget {
   const OrderActionButtons({super.key, required this.orderEntity});
 
   final OrderEntity orderEntity;
-
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -15,16 +16,37 @@ class OrderActionButtons extends StatelessWidget {
       children: [
         Visibility(
           visible: orderEntity.status == OrderStatus.pending,
-          child: ElevatedButton(onPressed: () {}, child: const Text('Accept')),
+          child: ElevatedButton(
+            onPressed: () {
+              context.read<UpdateOrderCubit>().updateOrder(
+                status: OrderStatus.accepted,
+                orderId: orderEntity.orderId,
+              );
+            },
+            child: const Text('Accept'),
+          ),
         ),
         Visibility(
           visible: orderEntity.status == OrderStatus.pending,
-          child: ElevatedButton(onPressed: () {}, child: const Text('Reject')),
+          child: ElevatedButton(
+            onPressed: () {
+              context.read<UpdateOrderCubit>().updateOrder(
+                status: OrderStatus.cancelled,
+                orderId: orderEntity.orderId,
+              );
+            },
+            child: const Text('Reject'),
+          ),
         ),
         Visibility(
           visible: orderEntity.status == OrderStatus.accepted,
           child: ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              context.read<UpdateOrderCubit>().updateOrder(
+                status: OrderStatus.delivered,
+                orderId: orderEntity.orderId,
+              );
+            },
             child: const Text('Delivered'),
           ),
         ),
